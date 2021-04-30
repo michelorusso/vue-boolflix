eb143a55d6ca095c18f363d572715b6edd6d5560
Michele Lorusso <kirito102095@hotmail.it>
2021-04-30 11:37:08 +0200

    Milestone 2 Complete

--- js/script.js
+++ js/script.js
@@ -8,6 +8,12 @@
 // -Lingua
 // -Voto
 
+// Milestone 2:
+// Trasformiamo la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+
+// Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
+// Qui un esempio di chiamata per le serie tv:
+
 var app = new Vue(
     {
     el: '#root',
@@ -18,7 +24,7 @@ var app = new Vue(
     methods: {
         searchMovie() {
 
-            axios.get('https://api.themoviedb.org/3/search/multi', {
+            axios.get('https://api.themoviedb.org/3/search/movie', {
                         params: {
                             api_key: 'e56155409e3774c5176290779eef0727',
                             query: this.title,
@@ -27,7 +33,8 @@ var app = new Vue(
                     })
                         .then((response) => {
 
-                            this.listMovie.push(response.data.results);
+                            const obj = response.data;
+                            this.listMovie.push(obj.results);
 
                             console.log('listMovie' ,this.listMovie)
                         });
