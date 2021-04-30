1d25ea4e69eff2622791ae7ddf14474b633eb776
Michele Lorusso <kirito102095@hotmail.it>
2021-04-30 14:54:21 +0200

    Milestone 3 Complete

--- js/script.js
+++ js/script.js
@@ -10,9 +10,15 @@
 
 // Milestone 2:
 // Trasformiamo la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
-
 // Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
-// Qui un esempio di chiamata per le serie tv:
+
+// Milestone 3:
+// In questa milestone come prima cosa aggiungiamo la copertina del film o della serie al nostro elenco.
+//  Ci viene passata dall’API solo la parte finale dell’URL, questo perché poi potremo generare da quella porzione di URL tante dimensioni diverse. Dovremo prendere quindi l’URL base delle immagini di TMDB: https://image.tmdb.org/t/p/ per poi aggiungere la dimensione che vogliamo generare per poi aggiungere la parte finale dell’URL passata dall’API.
+// Trasformiamo poi il voto da 1 a 10 decimale in un numero intero da 1 a 5, così da permetterci di stampare a schermo un numero di stelle piene che vanno da 1 a 5, lasciando le restanti vuote (troviamo le icone in FontAwesome).
+// Arrotondiamo sempre per eccesso all’unità successiva, non gestiamo icone mezze piene (o mezze vuote :P)
+
+
 
 var app = new Vue(
     {
@@ -28,19 +34,20 @@ var app = new Vue(
 
             // search Movie
             axios.get('https://api.themoviedb.org/3/search/movie', {
-                        params: {
-                            api_key: 'e56155409e3774c5176290779eef0727',
-                            query: this.title,
-                            page: 1,
-                        }
-                    })
-                        .then((response) => {
+                    params: {
+                        api_key: 'e56155409e3774c5176290779eef0727',
+                        query: this.title,
+                        page: 1,
+                    }
+                })
+                    .then((response) => {
 
-                            const obj = response.data;
-                            this.listMovie.push(obj.results);
+                        const obj = response.data;
+                        this.listMovie.push(obj.results);
 
-                            console.log('listMovie' ,this.listMovie)
-                        });
+                        console.log('listMovie' ,this.listMovie)
+                    });
+            
             
             // search Tv Series
             axios.get('https://api.themoviedb.org/3/search/tv', {
